a7cf08c0fa7c4d10344f1fa2f9dde2de5cd8d132
rafael <rafael@TI07LNX>
2020-11-06 17:33:57 -0300

    Inclusão de novos campos para tratar as devoluções de pessoas juridicas.

--- martinello-matriz-bd/src/main/java/br/com/martinello/matriz/bd/transients/PedidoTray.java
+++ martinello-matriz-bd/src/main/java/br/com/martinello/matriz/bd/transients/PedidoTray.java
@@ -17,25 +17,42 @@ public class PedidoTray {
     private Long pedidoId;
     private String carrinhoId;
     private Long situacaoPedidoId;
+    private String tipoRastreamentoPedido;
     private Date data;
+    private Date dataPagamento;
+    private Date dataUltimaAtualizacao;
+    private Double valorFrete;
+    private Double valorTotalPedido;
     private Double valorTotal;
     private Double valorJuros;
     private Double valorDesconto;
+    private Double valorDebitoCC;
+    private String marketPlacePedidoId;
+    private String marketPlacePedidoSiteId;
+    private String canalOrigem;
     private Long usuarioId;
     private Long enderecoId;
     private Boolean isMobile;
     private Long eventoId;
     /*Nome precisa ser assim para a conversão correta do GSON */
-    private List<PedidoProdutoTray> produtos;
+    private List<PedidoItemTray> itens;
     /*Nome precisa ser assim para a conversão correta do GSON */
     private List<PedidoFreteTray> fretes;
     /*Nome precisa ser assim para a conversão correta do GSON */
+    private List<PedidoAssinaturaTray> assinatura;
+    /*Nome precisa ser assim para a conversão correta do GSON */
     private List<PedidoPagamentoTray> pagamento;
+    private PedidoEnderecoTray pedidoEndereco;
     private Long canalId;
     private Long transacaoId;
-    private String observacao;
-    private Boolean valido;
     private String cupomDesconto;
+    private Long retiradaLojaId;
+    private boolean isPedidoEvento;
+    /*Nome precisa ser assim para a conversão correta do GSON */
+    private List<PedidoObservacaoTray> observacao;
+    private Double valorCreditoFidelidade;
+    private boolean valido;
+    private Double valorSubTotalSemDescontos;
 
     public Long getPedidoId() {
         return pedidoId;
@@ -61,12 +78,52 @@ public class PedidoTray {
         this.situacaoPedidoId = situacaoPedidoId;
     }
 
+    public String getTipoRastreamentoPedido() {
+        return tipoRastreamentoPedido;
+    }
+
+    public void setTipoRastreamentoPedido(String tipoRastreamentoPedido) {
+        this.tipoRastreamentoPedido = tipoRastreamentoPedido;
+    }
+
     public Date getData() {
         return data;
     }
 
     public void setData(Date data) {
         this.data = data;
+    }
+
+    public Date getDataPagamento() {
+        return dataPagamento;
+    }
+
+    public void setDataPagamento(Date dataPagamento) {
+        this.dataPagamento = dataPagamento;
+    }
+
+    public Date getDataUltimaAtualizacao() {
+        return dataUltimaAtualizacao;
+    }
+
+    public void setDataUltimaAtualizacao(Date dataUltimaAtualizacao) {
+        this.dataUltimaAtualizacao = dataUltimaAtualizacao;
+    }
+
+    public Double getValorFrete() {
+        return valorFrete;
+    }
+
+    public void setValorFrete(Double valorFrete) {
+        this.valorFrete = valorFrete;
+    }
+
+    public Double getValorTotalPedido() {
+        return valorTotalPedido;
+    }
+
+    public void setValorTotalPedido(Double valorTotalPedido) {
+        this.valorTotalPedido = valorTotalPedido;
     }
 
     public Double getValorTotal() {
@@ -91,6 +148,38 @@ public class PedidoTray {
 
     public void setValorDesconto(Double valorDesconto) {
         this.valorDesconto = valorDesconto;
+    }
+
+    public Double getValorDebitoCC() {
+        return valorDebitoCC;
+    }
+
+    public void setValorDebitoCC(Double valorDebitoCC) {
+        this.valorDebitoCC = valorDebitoCC;
+    }
+
+    public String getMarketPlacePedidoId() {
+        return marketPlacePedidoId;
+    }
+
+    public void setMarketPlacePedidoId(String marketPlacePedidoId) {
+        this.marketPlacePedidoId = marketPlacePedidoId;
+    }
+
+    public String getMarketPlacePedidoSiteId() {
+        return marketPlacePedidoSiteId;
+    }
+
+    public void setMarketPlacePedidoSiteId(String marketPlacePedidoSiteId) {
+        this.marketPlacePedidoSiteId = marketPlacePedidoSiteId;
+    }
+
+    public String getCanalOrigem() {
+        return canalOrigem;
+    }
+
+    public void setCanalOrigem(String canalOrigem) {
+        this.canalOrigem = canalOrigem;
     }
 
     public Long getUsuarioId() {
@@ -125,12 +214,12 @@ public class PedidoTray {
         this.eventoId = eventoId;
     }
 
-    public List<PedidoProdutoTray> getProdutos() {
-        return produtos;
+    public List<PedidoItemTray> getItens() {
+        return itens;
     }
 
-    public void setProdutos(List<PedidoProdutoTray> produtos) {
-        this.produtos = produtos;
+    public void setItens(List<PedidoItemTray> itens) {
+        this.itens = itens;
     }
 
     public List<PedidoFreteTray> getFretes() {
@@ -141,12 +230,28 @@ public class PedidoTray {
         this.fretes = fretes;
     }
 
+    public List<PedidoAssinaturaTray> getAssinatura() {
+        return assinatura;
+    }
+
+    public void setAssinatura(List<PedidoAssinaturaTray> assinatura) {
+        this.assinatura = assinatura;
+    }
+
     public List<PedidoPagamentoTray> getPagamento() {
         return pagamento;
     }
 
     public void setPagamento(List<PedidoPagamentoTray> pagamento) {
         this.pagamento = pagamento;
+    }
+
+    public PedidoEnderecoTray getPedidoEndereco() {
+        return pedidoEndereco;
+    }
+
+    public void setPedidoEndereco(PedidoEnderecoTray pedidoEndereco) {
+        this.pedidoEndereco = pedidoEndereco;
     }
 
     public Long getCanalId() {
@@ -165,28 +270,60 @@ public class PedidoTray {
         this.transacaoId = transacaoId;
     }
 
-    public String getObservacao() {
-        return observacao;
-    }
-
-    public void setObservacao(String observacao) {
-        this.observacao = observacao;
-    }
-
-    public Boolean getValido() {
-        return valido;
-    }
-
-    public void setValido(Boolean valido) {
-        this.valido = valido;
-    }
-
     public String getCupomDesconto() {
         return cupomDesconto;
     }
 
     public void setCupomDesconto(String cupomDesconto) {
         this.cupomDesconto = cupomDesconto;
+    }
+
+    public Long getRetiradaLojaId() {
+        return retiradaLojaId;
+    }
+
+    public void setRetiradaLojaId(Long retiradaLojaId) {
+        this.retiradaLojaId = retiradaLojaId;
+    }
+
+    public boolean isIsPedidoEvento() {
+        return isPedidoEvento;
+    }
+
+    public void setIsPedidoEvento(boolean isPedidoEvento) {
+        this.isPedidoEvento = isPedidoEvento;
+    }
+
+    public List<PedidoObservacaoTray> getObservacao() {
+        return observacao;
+    }
+
+    public void setObservacao(List<PedidoObservacaoTray> observacao) {
+        this.observacao = observacao;
+    }
+
+    public Double getValorCreditoFidelidade() {
+        return valorCreditoFidelidade;
+    }
+
+    public void setValorCreditoFidelidade(Double valorCreditoFidelidade) {
+        this.valorCreditoFidelidade = valorCreditoFidelidade;
+    }
+
+    public boolean isValido() {
+        return valido;
+    }
+
+    public void setValido(boolean valido) {
+        this.valido = valido;
+    }
+
+    public Double getValorSubTotalSemDescontos() {
+        return valorSubTotalSemDescontos;
+    }
+
+    public void setValorSubTotalSemDescontos(Double valorSubTotalSemDescontos) {
+        this.valorSubTotalSemDescontos = valorSubTotalSemDescontos;
     }
 
 }
